2d17da2976a014b3ce39812de48f2d78677e6994
codehome <dsyslove@163.com>
2020-09-13 09:45:47 +0800

    修改readme.md

--- src/main/java/vip/codehome/imghost/service/impl/SMMSImgHostService.java
+++ src/main/java/vip/codehome/imghost/service/impl/SMMSImgHostService.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.apache.commons.collections.MapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import vip.codehome.imghost.ImgHostProperties;
 import vip.codehome.imghost.common.UploadResult;
@@ -28,24 +30,30 @@ import vip.codehome.imghost.util.SMMSApiUtil;
  * "url":"https:\/\/i.loli.net\/2020\/09\/11\/Y2I5PcMpxiwWUav.jpg",
  * "delete":"https:\/\/sm.ms\/delete\/LfOxlyJESMaVnvbZtcQgw1Fjki",
  * "page":"https:\/\/sm.ms\/image\/Y2I5PcMpxiwWUav"},"RequestId":"3429CAA0-26D7-4F5B-BCA3-3AEF6EB23417"}
+ * {"success":false,"code":"image_repeated",
+ * "message":"Image upload repeated limit, this image exists at: https:\/\/i.loli.net\/2020\/09\/12\/dbs4LOUJghy7olm.jpg",
+ * "images":"https:\/\/i.loli.net\/2020\/09\/12\/dbs4LOUJghy7olm.jpg",
+ * "RequestId":"160F5EB9-970D-4405-85F7-D8F044DD2E1D"}
  */
 @Data
 @AllArgsConstructor
 public class SMMSImgHostService implements ImgHostService {
+    private final static Logger log= LoggerFactory.getLogger(SMMSImgHostService.class);
 	ImgHostProperties imgHostProperties;
 	@Override
     public UploadResult upload(File file) {
         String res=SMMSApiUtil.upload(imgHostProperties.getSmms().getToken(),file);
+        log.debug("图片{}上传请求结果为:{}",file.getName(),res);
         Map<String,Object> retMap=JSONUtil.toObject(res, new TypeReference<Map<String,Object>>() {
         });
+        Map<String,Object> tmpMap= MapUtils.getMap(retMap,"data");
         UploadResult uploadResult=new UploadResult();
         if("success".equals(MapUtils.getString(retMap,"code"))){
             uploadResult.setOk(true);
-            Map<String,Object> tmpMap= (Map<String, Object>) retMap.get("data");
             uploadResult.setUrl(tmpMap.get("url").toString());
-            uploadResult.setUrl("上传到sm.ms成功");
         }else{
             uploadResult.setOk(false);
+            uploadResult.setErrMsg(MapUtils.getString(retMap,"message"));
         }
         return uploadResult;
     }
